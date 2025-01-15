f2fa65ac05d30ae1f21806863b13842ca7218b12
dennis <info.dennis.durmus@gmail.com>
2025-01-15 14:50:36 +0100

    Refactor main.dart to remove MyApp and ThemeNotifier classes, and streamline application initialization

--- zeta_park/lib/main.dart
+++ zeta_park/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_application_1/screens/my_home_page.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'app.dart';
+import 'notifiers/theme_notifier.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,42 +25,4 @@ void main() async {
       child: const MyApp(),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final themeNotifier = Provider.of<ThemeNotifier>(context);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: themeNotifier.isDarkMode ? ThemeData.dark() : ThemeData.light(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Zeta Park Villalari Kusadasi',
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: Colors.white,
-        ),
-        body: const MyHomePage(),
-      ),
-    );
-  }
-}
-
-class ThemeNotifier extends ChangeNotifier {
-  bool _isDarkMode;
-
-  ThemeNotifier(this._isDarkMode);
-
-  bool get isDarkMode => _isDarkMode;
-
-  void toggleTheme() async {
-    _isDarkMode = !_isDarkMode;
-    notifyListeners();
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setBool('isDarkMode', _isDarkMode);
-  }
 }
