d9e1855b1fd6a39e0ec4c6d8ae948b3a437085fa
dennis <info.dennis.durmus@gmail.com>
2025-01-15 14:10:02 +0100

    Add shared_preferences support and update theme management

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/my_home_page.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  final isDarkMode = prefs.getBool('isDarkMode') ?? false;
+
   runApp(
     ChangeNotifierProvider(
-      create: (context) => ThemeNotifier(),
+      create: (context) => ThemeNotifier(isDarkMode),
       child: const MyApp(),
     ),
   );
@@ -35,12 +40,16 @@ class MyApp extends StatelessWidget {
 }
 
 class ThemeNotifier extends ChangeNotifier {
-  bool _isDarkMode = false;
+  bool _isDarkMode;
+
+  ThemeNotifier(this._isDarkMode);
 
   bool get isDarkMode => _isDarkMode;
 
-  void toggleTheme() {
+  void toggleTheme() async {
     _isDarkMode = !_isDarkMode;
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDarkMode', _isDarkMode);
   }
 }
