5f6b1328502cbc32d9eb08e15546fd1add7f37af
dennis <info.dennis.durmus@gmail.com>
2025-01-15 14:42:53 +0100

    Refactor MyHomePage layout to use SafeArea and SingleChildScrollView for better responsiveness

--- zeta_park/lib/screens/my_home_page.dart
+++ zeta_park/lib/screens/my_home_page.dart
@@ -49,127 +49,136 @@ class _MyHomePageState extends State<MyHomePage> {
     final List<int> years = List.generate(10, (index) => 2024 + index);
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            const CustomCard(),
-            const SizedBox(height: 16.0),
-            DropdownButton<String>(
-              value: selectedBlock,
-              items: blocks.map((String block) {
-                return DropdownMenuItem<String>(
-                  value: block,
-                  child: Text(block),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedBlock = newValue!;
-                  // Setze die Beträge für den ausgewählten Block
-                  betrag1 = 0.00;
-                  betrag2 = 0.00;
-                });
-              },
-              isExpanded: true,
-            ),
-            const SizedBox(height: 16.0),
-            Row(
-              children: [
-                Expanded(
-                  child: DropdownButton<String>(
-                    value: selectedMonth,
-                    items: months.map((String month) {
-                      return DropdownMenuItem<String>(
-                        value: month,
-                        child: Text(month),
-                      );
-                    }).toList(),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        selectedMonth = newValue!;
-                      });
-                    },
-                    isExpanded: true,
-                  ),
-                ),
-                const SizedBox(width: 8.0),
-                Expanded(
-                  child: DropdownButton<int>(
-                    value: selectedYear,
-                    items: years.map((int year) {
-                      return DropdownMenuItem<int>(
-                        value: year,
-                        child: Text(year.toString()),
-                      );
-                    }).toList(),
-                    onChanged: (int? newValue) {
-                      setState(() {
-                        selectedYear = newValue!;
-                      });
-                    },
-                    isExpanded: true,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16.0),
-            // TextField(
-            //   decoration: const InputDecoration(labelText: 'Aidat Ödemesi'),
-            //   keyboardType: TextInputType.number,
-            //   onChanged: _updateBetrag1,
-            // ),
-            // const SizedBox(height: 16.0),
-            // TextField(
-            //   decoration: const InputDecoration(labelText: 'Ek Ödemesi'),
-            //   keyboardType: TextInputType.number,
-            //   onChanged: _updateBetrag2,
-            // ),
-            const SizedBox(height: 16.0),
-            const Text(
-              'Site Yönetimi: Fatih Sevindik',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-            const SizedBox(height: 8.0),
-            //const Text('221B Baker Street, W1U 8ED'),
-            Divider(thickness: 1, color: Colors.grey[300]),
-            const SizedBox(height: 8.0),
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    const CustomCard(),
+                    const SizedBox(height: 16.0),
+                    DropdownButton<String>(
+                      value: selectedBlock,
+                      items: blocks.map((String block) {
+                        return DropdownMenuItem<String>(
+                          value: block,
+                          child: Text(block),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedBlock = newValue!;
+                          // Setze die Beträge für den ausgewählten Block
+                          betrag1 = 0.00;
+                          betrag2 = 0.00;
+                        });
+                      },
+                      isExpanded: true,
+                    ),
+                    const SizedBox(height: 16.0),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: DropdownButton<String>(
+                            value: selectedMonth,
+                            items: months.map((String month) {
+                              return DropdownMenuItem<String>(
+                                value: month,
+                                child: Text(month),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                selectedMonth = newValue!;
+                              });
+                            },
+                            isExpanded: true,
+                          ),
+                        ),
+                        const SizedBox(width: 8.0),
+                        Expanded(
+                          child: DropdownButton<int>(
+                            value: selectedYear,
+                            items: years.map((int year) {
+                              return DropdownMenuItem<int>(
+                                value: year,
+                                child: Text(year.toString()),
+                              );
+                            }).toList(),
+                            onChanged: (int? newValue) {
+                              setState(() {
+                                selectedYear = newValue!;
+                              });
+                            },
+                            isExpanded: true,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16.0),
+                    // TextField(
+                    //   decoration: const InputDecoration(labelText: 'Aidat Ödemesi'),
+                    //   keyboardType: TextInputType.number,
+                    //   onChanged: _updateBetrag1,
+                    // ),
+                    // const SizedBox(height: 16.0),
+                    // TextField(
+                    //   decoration: const InputDecoration(labelText: 'Ek Ödemesi'),
+                    //   keyboardType: TextInputType.number,
+                    //   onChanged: _updateBetrag2,
+                    // ),
+                    const SizedBox(height: 16.0),
+                    const Text(
+                      'Site Yönetimi: Fatih Sevindik',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    const SizedBox(height: 8.0),
+                    //const Text('221B Baker Street, W1U 8ED'),
+                    Divider(thickness: 1, color: Colors.grey[300]),
+                    const SizedBox(height: 8.0),
 
-            // PAYMENT METHOD
-            // const Text(
-            //   'Banka Dekont Giris Bilgileri',
-            //   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            // ),
-            // const SizedBox(height: 8.0),
-            // const Text('Visa\n**** **** **** 4243'),
-            // Divider(thickness: 1, color: Colors.grey[300]),
-            // const SizedBox(height: 8.0),
+                    // PAYMENT METHOD
+                    // const Text(
+                    //   'Banka Dekont Giris Bilgileri',
+                    //   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    // ),
+                    // const SizedBox(height: 8.0),
+                    // const Text('Visa\n**** **** **** 4243'),
+                    // Divider(thickness: 1, color: Colors.grey[300]),
+                    // const SizedBox(height: 8.0),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Aidat Ödemesi:'),
-                Text(
-                    '${Money.fromInt((betrag1 * 100).toInt(), code: selectedCurrency.code)}'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Ek Ödeme:'),
-                Text(
-                    '${Money.fromInt((betrag2 * 100).toInt(), code: selectedCurrency.code)}'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Toplam Ödeme:'),
-                Text(
-                    '${Money.fromInt((gesamtsumme * 100).toInt(), code: selectedCurrency.code)}'),
-              ],
-            ),
-          ],
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Aidat Ödemesi:'),
+                        Text(
+                            '${Money.fromInt((betrag1 * 100).toInt(), code: selectedCurrency.code)}'),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Ek Ödeme:'),
+                        Text(
+                            '${Money.fromInt((betrag2 * 100).toInt(), code: selectedCurrency.code)}'),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Toplam Ödeme:'),
+                        Text(
+                            '${Money.fromInt((gesamtsumme * 100).toInt(), code: selectedCurrency.code)}'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
