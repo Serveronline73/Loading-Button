32ef7dc81f01c67079da3297f822ccb4aa0aef20
dennis <info.dennis.durmus@gmail.com>
2025-01-18 18:46:55 +0100

    Refactor shared preferences helper methods for clarity and consistency

--- zeta_park/lib/repository/sharedPreferences.dart
+++ zeta_park/lib/repository/sharedPreferences.dart
@@ -1,50 +1,43 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesHelper {
-  // Speichert Nebenkosten unter dem angegebenen Schlüssel
+  // Speichert Nebenkosten
   static Future<void> saveNebenkosten(String key, String value) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString(key, value);
   }
 
-  // Lädt einen String-Wert unter dem angegebenen Schlüssel
-  static Future<String?> loadString(String key) async {
+  static Future<String?> loadBlock(String key) async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString(key);
   }
 
-  // Speichert einen booleschen Wert unter dem angegebenen Schlüssel
-  static Future<void> saveBool(String key, bool value) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(key, value);
-  }
+  // static Future<void> saveBool(String key, bool value) async {
+  //   final prefs = await SharedPreferences.getInstance();
+  //   await prefs.setBool(key, value);
+  // }
 
-  // Lädt einen booleschen Wert unter dem angegebenen Schlüssel
-  static Future<bool?> loadBool(String key) async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(key);
-  }
+  // static Future<bool?> loadBool(String key) async {
+  //   final prefs = await SharedPreferences.getInstance();
+  //   return prefs.getBool(key);
+  // }
 
-  // Speichert einen Integer-Wert unter dem angegebenen Schlüssel
-  static Future<void> saveInt(String key, int value) async {
+  static Future<void> saveYear(String key, int value) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setInt(key, value);
   }
 
-  // Lädt einen Integer-Wert unter dem angegebenen Schlüssel
-  static Future<int?> loadInt(String key) async {
+  static Future<int?> loadYear(String key) async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getInt(key);
   }
 
-  // Speichert einen Double-Wert unter dem angegebenen Schlüssel
-  static Future<void> saveDouble(String key, double value) async {
+  static Future<void> saveBetrag(String key, double value) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setDouble(key, value);
   }
 
-  // Lädt einen Double-Wert unter dem angegebenen Schlüssel
-  static Future<double?> loadDouble(String key) async {
+  static Future<double?> loadBetrag(String key) async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getDouble(key);
   }
