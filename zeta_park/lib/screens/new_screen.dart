b9630bfd27bfde234b384618356a1a744a390043
dennis <info.dennis.durmus@gmail.com>
2025-01-19 12:53:27 +0100

    Update import path for DataManager and enhance comments in NewScreen

--- zeta_park/lib/screens/new_screen.dart
+++ zeta_park/lib/screens/new_screen.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/data/data_manager.dart';
+import 'package:flutter_application_1/repository/data_manager.dart';
 import 'package:flutter_application_1/screens/item_detail_screen.dart';
 import 'package:money2/money2.dart';
 
 class NewScreen extends StatelessWidget {
   NewScreen({super.key});
 
-  final Currency selectedCurrency = Currency.create('TRY', 2, symbol: '₺');
-  final DataManager _dataManager = DataManager();
+  final Currency selectedCurrency =
+      Currency.create('TRY', 2, symbol: '₺'); // Türkische währung in Lira
+  final DataManager _dataManager = DataManager(); //
 
   @override
   Widget build(BuildContext context) {
     final List<Map<String, String>> items = List.generate(
-      50,
+      // Liste von Blöcken
+      50, // Anzahl der Blöcke
       (i) {
-        final int blockNumber = (i ~/ 2) + 1;
-        final String suffix = (i % 2 == 0) ? '' : 'a';
+        final int blockNumber = (i ~/ 2) + 1; // Blocknummer berechnen
+        final String suffix = (i % 2 == 0) ? '' : 'a'; // Suffix berechnen
         return {
-          'title': 'Zeta Park Blok 1/$blockNumber$suffix',
+          'title':
+              'Zeta Park Blok 1/$blockNumber$suffix', // Blocktitel mit Blocknummer und Suffix
           'subtitle': 'Aidat ödeme ve Ek Ödeme Bilgileri.',
         };
       },
@@ -48,16 +51,22 @@ class NewScreen extends StatelessWidget {
                 color: Colors.black,
                 child: ListView.builder(
                   shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
+                  physics:
+                      const NeverScrollableScrollPhysics(), // Scrollen deaktivieren
                   itemCount: items.length,
                   itemBuilder: (context, index) {
-                    final blockAmounts = _dataManager.getBlockAmounts();
+                    final blockAmounts = _dataManager
+                        .getBlockAmounts(); // Blockbeträge aus dem DataManager holen
                     return Card(
+                      // Karte für jeden Block erstellen
                       child: ListTile(
+                        // Liste für jeden Block erstellen
                         leading: Icon(
                           Icons.house,
                           size: 60,
-                          color: colors[index % colors.length],
+                          color: colors[index %
+                              colors
+                                  .length], // Farbe für jeden Block aus der Liste wählen
                         ),
                         title: Text(
                           items[index]['title']!,
@@ -66,9 +75,12 @@ class NewScreen extends StatelessWidget {
                         ),
                         subtitle: Text(items[index]['subtitle']!),
                         onTap: () {
+                          // Karte klicken -> Blockdetails anzeigen
                           Navigator.push(
+                            // Navigation zu ItemDetailScreen
                             context,
                             MaterialPageRoute(
+                              // Navigation zu ItemDetailScreen
                               builder: (context) => ItemDetailScreen(
                                 title: items[index]['title']!,
                                 subtitle: items[index]['subtitle']!,
