b1d264fada21737312ce1f4d16fea7a1eeb61d69
dennis <info.dennis.durmus@gmail.com>
2025-01-20 21:20:36 +0100

    Refactor MyApp to use MultiProvider and FutureBuilder for login status check

--- zeta_park/lib/app.dart
+++ zeta_park/lib/app.dart
@@ -1,25 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/notifiers/login_notifier.dart'; // Import LoginNotifier
 import 'package:flutter_application_1/screens/login_screen.dart';
 import 'package:flutter_application_1/screens/my_home_page.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'notifiers/theme_notifier.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  Future<bool> _checkLoginStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('isLoggedIn') ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final themeNotifier = Provider.of<ThemeNotifier>(context);
-    const bool isLoggedIn =
-        true; // Vorübergehend auf false gesetzt, bis die Authentifizierung implementiert ist danach wieder auf true setzen
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: themeNotifier.isDarkMode ? ThemeData.dark() : ThemeData.light(),
-      home: isLoggedIn
-          ? const MyHomePage()
-          : const LoginScreen(), // Conditional navigation
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (_) => LoginNotifier()), // Provide LoginNotifier
+        ChangeNotifierProvider(create: (_) => ThemeNotifier(false)),
+      ],
+      child: Consumer<ThemeNotifier>(
+        builder: (context, themeNotifier, child) {
+          return FutureBuilder<bool>(
+            future: _checkLoginStatus(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else {
+                final bool isLoggedIn = snapshot.data ?? false;
+                return MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  theme: themeNotifier.isDarkMode
+                      ? ThemeData.dark()
+                      : ThemeData.light(),
+                  home: isLoggedIn
+                      ? const MyHomePage()
+                      : const LoginScreen(), // Conditional navigation
+                  routes: {
+                    '/login': (context) => const LoginScreen(),
+                    '/home': (context) => const MyHomePage(),
+                  },
+                );
+              }
+            },
+          );
+        },
+      ),
     );
   }
 }
