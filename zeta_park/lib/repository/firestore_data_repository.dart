6d6b673030cf3391ad1252242138155133e8c1e7
dennis <info.dennis.durmus@gmail.com>
2025-01-27 23:00:08 +0100

    Add FirestoreDataRepository for managing expenses and payments

---
+++ zeta_park/lib/repository/firestore_data_repository.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_application_1/models/expense.dart';
+import 'package:flutter_application_1/models/house.dart';
+import 'package:flutter_application_1/models/payment.dart';
+
+class FirestoreDataRepository {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Future<List<Expense>> getExpenses({required House house}) async {
+    try {
+      QuerySnapshot querySnapshot =
+          await _firestore.collection('${house.id}/expenses').get();
+
+      // Extrahiere die Daten aus den Dokumenten
+      List<Expense> expenses = querySnapshot.docs.map((doc) {
+        return Expense.fromMap(doc.data() as Map<String, dynamic>);
+      }).toList();
+      return expenses;
+    } catch (e) {
+      print('Fehler beim Abrufen der Daten: $e');
+      return [];
+    }
+  }
+
+  Future<void> addExpense(
+      {required Expense expense, required House house}) async {
+    try {
+      await _firestore.collection('${house.id}/expenses').add(expense.toMap());
+    } catch (e) {
+      print('Fehler beim Hinzufügen der Ausgabe: $e');
+    }
+  }
+
+  Future<List<Payment>> getPayments({required House house}) async {
+    try {
+      QuerySnapshot querySnapshot =
+          await _firestore.collection('${house.id}/payments').get();
+
+      // Extrahiere die Daten aus den Dokumenten
+      List<Payment> payments = querySnapshot.docs.map((doc) {
+        return Payment.fromMap(doc.data() as Map<String, dynamic>);
+      }).toList();
+      return payments;
+    } catch (e) {
+      print('Fehler beim Abrufen der Daten: $e');
+      return [];
+    }
+  }
+
+  Future<void> addPayment(
+      {required Payment payment, required House house}) async {
+    try {
+      await _firestore.collection('${house.id}/payments').add(payment.toMap());
+    } catch (e) {
+      print('Fehler beim Hinzufügen der Zahlung: $e');
+    }
+  }
+}
